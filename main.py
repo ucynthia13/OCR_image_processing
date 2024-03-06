1b0c7b586c199a6e8c747658a2797672cc55dc65
ucynthia13 <cycyu614@gmail.com>
2024-03-06 10:27:44 +0200

    image-training

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import cv2
-import pytesseract
+import os
+import numpy as np
+from PIL import Image
 
-# Configure Tesseract path (update if needed)
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
+# Create LBPH Face Recognizer
+#pip install opencv-contrib-python
+recognizer = cv2.face.LBPHFaceRecognizer.create()
 
-# Load the image
-image = cv2.imread("./image.png")
+# Load the Haar Cascade Classifier for face detection
+detector = cv2.CascadeClassifier("./model/haarcascade_frontalface_default.xml")
 
-# Preprocess the image
-grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-thresholded_image = cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+def getImagesAndLabels(path):
+    imagePaths = [os.path.join(path, f) for f in os.listdir(path) if not f.startswith('.')]
+    faceSamples = []
+    Ids = []
+    for imagePath in imagePaths:
+        try:
+            pilImage = Image.open(imagePath).convert('L')
+            imageNp = np.array(pilImage, 'uint8')
+            Id = int(os.path.split(imagePath)[-1].split(".")[1])
+            faces = detector.detectMultiScale(imageNp)
+            for (x, y, w, h) in faces:
+                faceSamples.append(imageNp[y:y + h, x:x + w])
+                Ids.append(Id)
+        except Exception as e:
+            print(f"Error processing image {imagePath}: {e}")
+    return faceSamples, Ids
 
-# Find contours (adjust for potential multiple digits)
-contours = cv2.findContours(thresholded_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-contours = contours[0] if len(contours) == 2 else contours[1]
-
-# Extract the largest region of interest (ROI)
-largest_contour = max(contours, key=cv2.contourArea)
-x, y, w, h = cv2.boundingRect(largest_contour)
-roi = image[y:y+h, x:x+w]
-
-# Perform OCR on the ROI
-extracted_text = pytesseract.image_to_string(roi, config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
-
-# Display the extracted text
-print(extracted_text)
+faces, Ids = getImagesAndLabels('./model/dataset')
+recognizer.train(faces, np.array(Ids))
+recognizer.save("./model/trained_lbph_face_recognizer_model.yml")
